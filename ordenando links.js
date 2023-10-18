fef63cb1bdf04c2d19ffe1b242195441a0e329ac
micalela <mica.berezan@gmail.com>
2023-10-17 23:57:54 -0300

    buenas noches

--- ordenando links.js
+++ ordenando links.js
@@ -1,190 +1,196 @@
 const linksydesc = [
-    {
-        "url": "https://new.math.uiuc.edu/oldnew",
-        "desc": "una buena página para explorar"
-    },
-    {
-        "url": "http://new.math.uiuc.edu/oldnew/optiverse/",
-        "desc": "acá nacieron los legendarios videos de cómo dar vuelta una esfera. hay un montón de material original de los videos y muchas reliquias para chusmear"
-    },
-    {
-        "url": "https://new.math.uiuc.edu/oldnew/optiverse/tshirts.html",
-        "desc": "cuando quieran coordinamos y les mandamos un mail, a ver si nos conseguimos unas remeras para el cmat"
-    },
-    {
-        "url": "https://new.math.uiuc.edu/zipproof/conwaypng/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.vanderbilt.edu/AnS/psychology/cogsci/chaos/workshop/Fractals.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.wahl.org/fe/HTML_version/link/FE4W/c4.htm",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.rogmann.org/math/tori/torus2en.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.kleinbottle.com/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://personal.math.ubc.ca/~cass/courses/m308-03b/projects-03b/skinner/ex-dimension-koch_snowflake.htm",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://personal.math.ubc.ca/~cass/courses/m308-03b/projects-03b/skinner/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www2.karlin.mff.cuni.cz/~pyrih/e/e2001v2/c/ect/node52.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://web1.kcn.jp/hp28ah77/index.html#chapters",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://web1.kcn.jp/hp28ah77/us27i_klpr.htm",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www2.karlin.mff.cuni.cz/~pyrih/e/e2001v2/c/ect/node1.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.complang.tuwien.ac.at/schani/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.math.leidenuniv.nl/~desmit/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://pi.math.cornell.edu/~dtaimina/hypplanes.htm",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.georgehart.com/virtual-polyhedra/vp.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.georgehart.com/index.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.cs.ubc.ca/~tmm/gc/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.geometrygames.org/HyperbolicBlanket/index.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.geometrygames.org/TorusGames/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.biscuitsandjam.com/stripe_maker.php",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://madeleineshepherd.blogspot.com/search/label/random%20knitting",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://themathematiciansshirts.wordpress.com/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.woollythoughts.com/borromeancubes.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.woollythoughts.com/dragons.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.mentalblocks.co.uk/aboutus.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.rogmann.org/archiv/math/hyp/Dreiecke.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.rogmann.org/archiv/math/hyp/anim/Animationen.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://aleph0.clarku.edu/~djoyce/poincare/poincare.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.ics.uci.edu/~eppstein/junkyard/hyper.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.geometrygames.org/HyperbolicGames/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.ics.uci.edu/~eppstein/junkyard/tiling.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://www.rogmann.org/math/csaszar/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://mathcurve.com/surfaces.gb/paraboloidhyperbolic/paraboloidhyperbolic.shtml",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://mathpuzzle.com/",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://kleinbottle.com/?utm_source=substack&utm_medium=email",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://mathcurve.com/surfaces.gb/mobiussurface/mobiussurface.shtml",
-        "desc": "descripcion"
-    },
-    {
-        "url": "http://andrewbayly.com/PR/pr.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.epsilones.com/paginas/laboratorio/laboratorio-002-teseladopenrose.html",
-        "desc": "descripcion"
-    },
-    {
-        "url": "https://www.epsilones.com/paginas/0-bestiario/bestiario-000.html",
-        "desc": "descripcion"
-    }
-]
-
+  {
+    url: "https://new.math.uiuc.edu/oldnew",
+    desc: "una buena página para explorar",
+  },
+  {
+    url: "http://new.math.uiuc.edu/oldnew/optiverse/",
+    desc: "acá nacieron los legendarios videos de cómo dar vuelta una esfera. hay un montón de material original de los videos y muchas reliquias para chusmear",
+  },
+  {
+    url: "https://new.math.uiuc.edu/oldnew/optiverse/tshirts.html",
+    desc: "cuando quieran coordinamos y les mandamos un mail, a ver si nos conseguimos unas remeras para el cmat",
+  },
+  {
+    url: "https://new.math.uiuc.edu/zipproof/conwaypng/",
+    desc: "una página con algunas ilustraciones de clasificación de superficies",
+  },
+  {
+    url: "https://www.vanderbilt.edu/AnS/psychology/cogsci/chaos/workshop/Fractals.html",
+    desc: "un poquito sobre fractales",
+  },
+  {
+    url: "https://www.wahl.org/fe/HTML_version/link/FE4W/c4.htm",
+    desc: "más fractales",
+  },
+  {
+    url: "http://www.rogmann.org/math/tori/torus2en.html",
+    desc: "cómo coser un bitoro",
+  },
+  {
+    url: "https://www.kleinbottle.com/",
+    desc: "una página completamente épica sobre botellas de klein",
+  },
+  {
+    url: "https://personal.math.ubc.ca/~cass/courses/m308-03b/projects-03b/skinner/ex-dimension-koch_snowflake.htm",
+    desc: "calculando la dimensión fractal de la curva de koch (sacar)",
+  },
+  {
+    url: "https://personal.math.ubc.ca/~cass/courses/m308-03b/projects-03b/skinner/",
+    desc: "sacar",
+  },
+  {
+    url: "https://www2.karlin.mff.cuni.cz/~pyrih/e/e2001v2/c/ect/node52.html",
+    desc: "una entrada del solenoide en una página de continuos localmente conexos",
+  },
+  {
+    url: "http://web1.kcn.jp/hp28ah77/index.html#chapters",
+    desc: "una de mis páginas favoritas de esta colección. la presento con palabras del autor: 'You know that we can enjoy a masterpiece of painting even though we can not paint it ourselves. Everybody has the right to enjoy true mathematics even if one is not familiar with math. It is not necessary for sightseers to know the laws or rules of the region being visited. All we have to do is to see how the mountains look and how the rivers run. I began writing this webpage without any knowledge of hyperbolic geometry. I pursued whatever came to my mind on a given occasion. Sometimes I accepted a mathematician's idea without question. Anyway, the Hyperbolic Non-Euclidean World is very mysterious and captivating, so have fun! Let's take off on an academic sight-seeing flight, and enjoy panoramic views of infinity!'",
+  },
+  {
+    url: "http://web1.kcn.jp/hp28ah77/us27i_klpr.htm",
+    desc: "esta es la entrada con la que descubrí la página",
+  },
+  {
+    url: "https://www2.karlin.mff.cuni.cz/~pyrih/e/e2001v2/c/ect/node1.html",
+    desc: "sacar",
+  },
+  {
+    url: "http://www.complang.tuwien.ac.at/schani/",
+    desc: "descripcion",
+  },
+  {
+    url: "https://www.math.leidenuniv.nl/~desmit/",
+    desc: "descripcion",
+  },
+  {
+    url: "http://pi.math.cornell.edu/~dtaimina/hypplanes.htm",
+    desc: "todo sobre crochet hiperbólico. otra de mis páginas favoritas de la colección",
+  },
+  {
+    url: "https://www.georgehart.com/virtual-polyhedra/vp.html",
+    desc: "un montón de información sobre poliedros además de muchas técnicas para construirlos",
+  },
+  {
+    url: "https://www.georgehart.com/index.html",
+    desc: "la página del señor de los poliedros",
+  },
+  {
+    url: "https://www.cs.ubc.ca/~tmm/gc/",
+    desc: "más material de geometry center. acá hay un pdf, 'Making Waves: A Guide to the Ideas behind Outside In', que me parece bastante icónico",
+  },
+  {
+    url: "https://www.geometrygames.org/HyperbolicBlanket/index.html",
+    desc: "una página que explica cómo hacer un poncho hiperbólico. juro solemnemente ponerme las pilas y hacer uno en algún momento",
+  },
+  {
+    url: "https://www.geometrygames.org/TorusGames/",
+    desc: "juegos que buscan introducir el concepto del toro como cociente",
+  },
+  {
+    url: "http://www.biscuitsandjam.com/stripe_maker.php",
+    desc: "sacar",
+  },
+  {
+    url: "http://madeleineshepherd.blogspot.com/search/label/random%20knitting",
+    desc: "tejidos aleatorios, me pareció un buen concepto",
+  },
+  {
+    url: "https://themathematiciansshirts.wordpress.com/",
+    desc: "sacar",
+  },
+  {
+    url: "http://www.woollythoughts.com",
+    desc: "una página sobre artes textiles y matemática",
+  },
+  {
+    url: "http://www.woollythoughts.com/dragons.html",
+    desc: "sacar",
+  },
+  {
+    url: "http://www.mentalblocks.co.uk/aboutus.html",
+    desc: "sacar",
+  },
+  {
+    url: "http://www.rogmann.org/math/hyperbolic.html",
+    desc: "una página sobre teselaciones hiperbólicas, de acá sale el tutorial de cómo coser un bitoro",
+  },
+  {
+    url: "http://www.rogmann.org/archiv/math/hyp/anim/Animationen.html",
+    desc: "sacar",
+  },
+  {
+    url: "http://aleph0.clarku.edu/~djoyce/poincare/poincare.html",
+    desc: "más teselaciones hiperbólicas, yendo para atrás en esta página hay un montón de cosas",
+  },
+  {
+    url: "http://www.cabri.net/abracadabri/WabraGene/abraGene.html",
+    desc: "parte de un webring de matemáticos de habla francesa, no he encontrado más páginas del anillo todavía",
+  },
+  {
+    url: "https://www.geometrygames.org/HyperbolicGames/",
+    desc: "otra como los torus games",
+  },
+  {
+    url: "https://www.ics.uci.edu/~eppstein/junkyard/tiling.html",
+    desc: "sacar",
+  },
+  {
+    url: "http://www.rogmann.org/math/csaszar/",
+    desc: "plegando papeles para obtener un poliedro de Császár",
+  },
+  {
+    url: "https://mathcurve.com/surfaces.gb/paraboloidhyperbolic/paraboloidhyperbolic.shtml",
+    desc: "una página hermosa que muestra un montón de superficies",
+  },
+  {
+    url: "http://mathpuzzle.com/",
+    desc: "tópicos en puzzles y matemática",
+  },
+  {
+    url: "https://kleinbottle.com/?utm_source=substack&utm_medium=email",
+    desc: "descripcion",
+  },
+  {
+    url: "https://mathcurve.com/surfaces.gb/mobiussurface/mobiussurface.shtml",
+    desc: "descripcion",
+  },
+  {
+    url: "http://andrewbayly.com/PR/pr.html",
+    desc: "descripcion",
+  },
+  {
+    url: "https://www.epsilones.com/paginas/laboratorio/laboratorio-002-teseladopenrose.html",
+    desc: "descripcion",
+  },
+  {
+    url: "https://www.epsilones.com/paginas/0-bestiario/bestiario-000.html",
+    desc: "descripcion",
+  },
+  {
+    url: "https://members.tripod.com/professor_tom/galleries/index.html",
+    desc: "galería de sistemas dinámicos",
+  },
+  {
+    url: "https://members.tripod.com/professor_tom/hyperbolic/index.html",
+    desc: "una página sobre superficies hiperbólicas",
+  },
+];
 
 const et = document.getElementById("lista de links");
 
-for (var i = 0; i < linksydesc.length; i++){
+for (var i = 0; i < linksydesc.length; i++) {
+  const item = document.createElement("dt");
+  const pg = document.createElement("a");
+  const desc = document.createElement("dd");
+  const br = document.createElement("br");
+  pg.setAttribute("href", linksydesc[i].url);
+  pg.setAttribute("target", "_blank");
+  pg.innerHTML = linksydesc[i].url; /* i.url */
+  desc.innerHTML = linksydesc[i].desc; /* i.desc */
 
-    const item = document.createElement("dt"); 
-    const pg = document.createElement("a");
-    const desc = document.createElement("dd");
-    const br = document.createElement("br")
-    pg.setAttribute("href", linksydesc[i].url);
-    pg.setAttribute("target", "_blank");
-    pg.innerHTML = linksydesc[i].url; /* i.url */
-    desc.innerHTML = linksydesc[i].desc; /* i.desc */
-
-    item.appendChild(pg);
-    et.appendChild(item);
-    et.appendChild(desc);
-    et.appendChild(br);
+  item.appendChild(pg);
+  et.appendChild(item);
+  et.appendChild(desc);
+  et.appendChild(br);
 }
