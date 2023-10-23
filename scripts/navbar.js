fc8ae82bb51eecf7d5ec3097fd823855fa90ceca
micalela <mica.berezan@gmail.com>
2023-10-22 22:47:45 -0300

    buenas noches

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,6 +2,7 @@ const navbar = `<div class="navbar">
 <a href="cartelitos.html">colescion de cosas</a>
 <a href="aprendiendo uwu.html">conocimiento</a>
 <a href="htmlmat2.html">paginas epicas</a>
+<a href="ascii.html">ascii</a>
 </div>`;
 
 const body = document.body;
