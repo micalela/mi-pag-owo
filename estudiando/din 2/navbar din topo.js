4cc6f35cde3a80a9b189aafcd34744654c4a4fc1
binbow <binbows94@gmail.com>
2024-05-01 22:09:49 -0300

    buenas noches

---
+++ estudiando/din 2/navbar din topo.js
@@ -0,0 +1,9 @@
+const navbar = `<div class="navbar">
+<a href="1.html">uwu</a>
+<span>✧</span>
+<a href="../../htmlmat2.html">atrás!</a>
+</div>`;
+
+const body = document.body;
+
+body.insertAdjacentHTML("beforeBegin", navbar);
