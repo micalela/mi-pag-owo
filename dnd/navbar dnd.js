14ad5b3fae62f19568745db016ec4f8a7ce46676
binbow <binbows94@gmail.com>
2024-05-30 23:58:02 -0300

    buenas noches

---
+++ dnd/navbar dnd.js
@@ -0,0 +1,9 @@
+const navbar = `<div class="navbar">
+<a href="dump 2.html">dump 2</a>
+<span>✧</span>
+<a href="../../ascii.html">atrás!</a>
+</div>`;
+
+const body = document.body;
+
+body.insertAdjacentHTML("beforeBegin", navbar);
