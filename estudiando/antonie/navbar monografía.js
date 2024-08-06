7afbda93ce711a3acba3300ce8706f44ef0d9c8e
binbow <binbows94@gmail.com>
2024-08-06 05:31:11 -0300

    buenas noches

--- estudiando/antonie/navbar monografía.js
+++ estudiando/antonie/navbar monografía.js
@@ -1,9 +1,19 @@
 const navbar = `<div class="navbar">
-<a href="preliminares.html">preliminares</a>
+<a href="conjuntos hiperbólicos.html">conjuntos hiperbólicos</a>
 <span>✧</span>
-<a href="../../htmlmat2.html">atrás!</a>
-</div>`;
+<a href="ifs.html">ifs</a>
+<span>✧</span>
+<a href="../../htmlmat2.html">atrás!</a></div><br>`;
+
+/* <button id="boton_navbar">✧˖°˖✧°</button> */
+
+/* const boton = document.createElement("button").innertext="hola"; */
 
 const body = document.body;
 
 body.insertAdjacentHTML("beforeBegin", navbar);
+
+const boton_navbar = document.getElementById("boton_navbar");
+
+boton_navbar.onclick=()=>{document.querySelectorAll('style, link[rel="stylesheet"]').forEach(e => e.remove());}
+
