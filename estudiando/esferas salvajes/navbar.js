c36fad7be178bc012594b77a24c1a531713acf50
micalela <mica.berezan@gmail.com>
2025-04-01 05:43:20 -0300

    buenas noches

--- estudiando/esferas salvajes/navbar.js
+++ estudiando/esferas salvajes/navbar.js
@@ -1,20 +1,6 @@
 const navbar = `<div class="navbar">
 <a href="comienza una nueva aventura.html">comienza una nueva aventura</a>
 <span>✧</span>
-<a href="petit brouwer.html">petit brouwer</a>
-<span>✧</span>
-<a href="traslación.html">traslación de brouwer</a>
-<span>✧</span>
-<a href="littlewood.html">cartwright-littlewood</a>
-<span>✧</span>
-<a href="pbf.html">poincaré-birkhoff-franks</a>
-<span>✧</span>
-<a href="foliado.html">brouwer foliado</a>
-<span>✧</span>
-<a href="homeos de superficie y foliaciones.html">homeos de superficie</a>
-<span>✧</span>
-<a href="cubrimientos del anillo.html">cubrimientos del anillo</a>
-<span>✧</span>
 <a href="../../htmlmat2.html">atrás!</a>
 </div>`;
 
