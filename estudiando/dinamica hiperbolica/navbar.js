5d84290cdf53553ac318bf4ec42a10c70b0ec1f9
micalela <mica.berezan@gmail.com>
2026-01-30 07:27:42 -0300

    buenas noches

---
+++ estudiando/dinamica hiperbolica/navbar.js
@@ -0,0 +1,13 @@
+const navbar = `<div class="navbar">
+<a href="intro.html">intro</a>
+<span>✧</span>
+<a href="hartman.html">hartman</a>
+<span>✧</span>
+<a href="anosov.html">anosov lineales</a>
+<span>✧</span>
+<a href="../../htmlmat2.html">atrás!</a>
+</div>`;
+
+const body = document.body;
+
+body.insertAdjacentHTML("beforeBegin", navbar);
