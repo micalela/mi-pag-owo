653a9f992d1d3cf9db8e6895bee48e6d04cb7f28
micalela <mica.berezan@gmail.com>
2026-01-31 06:00:38 -0300

    buenas noches

--- estudiando/dinamica hiperbolica/navbar.js
+++ estudiando/dinamica hiperbolica/navbar.js
@@ -1,9 +1,13 @@
 const navbar = `<div class="navbar">
-<a href="intro.html">intro</a>
+<a href="intro.html">caso lineal</a>
 <span>✧</span>
 <a href="hartman.html">hartman</a>
 <span>✧</span>
 <a href="anosov.html">anosov lineales</a>
+<span>✧</span>
+<a href="herradura.html">herradura y homoclinicos</a>
+<span>✧</span>
+<a href="difeos.html">difeos</a>
 <span>✧</span>
 <a href="../../htmlmat2.html">atrás!</a>
 </div>`;
